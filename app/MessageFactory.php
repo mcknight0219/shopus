067a8bb6571217a47f1fe2447b2b70858badc165
mcknight0219 <mcknight0219@gmail.com>
2016-01-21 16:36:24 -0700

    make outbound message able to generate xml output

--- app/MessageFactory.php
+++ app/MessageFactory.php
@@ -2,10 +2,10 @@
 
 namespace App;
 
-use Message;
-use Inbound;
-use Outbound;
-use Event;
+use Models\Message;
+use Models\Inbound;
+use Models\Outbound;
+use Models\Event;
 
 use App\Exceptions\MessageFactoryException;
 
@@ -13,30 +13,107 @@ class MessageFactory
 {
     public function create($type, $attributes)
     {
-        switch $type {
-            case 'inbound':
-                return $this->_createInbound($attributes);
-            case 'outbound':
-                return $this->_createOutbound($attributes);
-            case 'event':
-                return $this->_createEvent($attributes);
-            default:
-                throw new MessageFactoryException('Unknown message type');
+        $this->_guardParam($attributes);
+        if( 'inbound' === $type ) {
+            return $this->_createInbound($attributes);
+        } 
+        else if( 'outbound' === $type ) {
+            return $this->_createOutbound($attributes);
+        }
+        else if( 'event' === $type ) {
+            return $this->_createEvent($attributes);
+        }
+        else {
+            throw new MessageFactoryException('Unknown message type');
         }
     }
 
+    protected function _guardParam($params)
+    {
+        if( !is_array($params) || count($params) === 0 )
+            throw new MessageFactoryException('Parameter is not array or is empty');
+    }
+
+    // common fields for all type of messages
+    protected $_commonAttrs = ['ToUserName', 'FromUserName', 'CreateTime', 'MsgType'];
+
+    protected function _createBasic($attriubtes)
+    {
+        try {
+            $message = new Message;
+            $message->toUserName = $attributes['ToUserName'];
+            $message->fromUserName = $attriubtes['FromUserName'];
+            $message->createTime = $this->_epochToTimestamp($attributes['CreateTime']);
+            $message->type = $attributes['MsgType'];
+            
+            $message->save();
+            return $message;
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+        }   
+    }
+
+    // Message sent to us by normal subscribers
     protected function _createInbound($attributes)
     {   
-        
+        $message = $this->_createBasic($attributes);
+
+        $inbound = new Inbound;
+        $inbound->msgId = $attriubtes['MsgId'];
+        $inbound->content = json_encode(
+            array_filter($attributes, function predicate($a) {
+                return !in_array($a, array_merge($this->_commonAttrs, ['MsgId']));
+            })
+        );
+        $inbound->save();
+
+        $inbound->message()->save($message);
+        return $message;
     }
 
+    // Response we sent to subscribers' message
+    //
+    // Outbound message is more complex than inbound and event.
+    // we leave the burden of parsing to outbound model 
     protected function _createOutbound($attributes)
     {
+        $message = $this->_createBasic($attributes);
 
+        $outbound = new Outbound;
+        $outbound->content = json_encode(
+            array_filter($attributes, function predicate($a) {
+                return !in_array($a, $this->_commonAttrs);
+            })
+        );
+        $outbound->save();
+
+        $outbound->message()->save($message);
+        return $message;
     }
 
-    protected function _createEvent($attributes)
+    // When events such as subscribing, menu-clicking happen, we
+    // receive message from weixin
+    protected function _createEvent(Array $attributes)
     {
+        $message = $this->_createBasic($attributes);
 
+        $event = new Event;
+        $event->event = strtolower($attributes['Event']);
+        $event->eventKey = array_key_exists('EventKey', $attributes) ? $attributes['EventKey'] : null;
+        $event->ticket = array_key_exists('Ticket', $attributes) ? $attributes['Ticket'] : null;
+        // For LOCATION event, pack Latitude, Longitude, and Precision in eventKey
+        if (array_key_exists('Latitude', $attributes)) {
+            $event->eventKey = array($attributes['Latitude'], $attributes['Longitude'], $attributes['Precision']).implode(';');
+        }
+        $event->save();
+    
+        $event->message()->save($message);
+        return $message;
+    }
+
+    protected function _epochToTimestamp($epoch)
+    {
+        $dt = new DateTime(intval($epoch));
+        return $dt->format('Y-m-d H:i:s');        
     }
 }
--- app/Models/Outbound.php
+++ app/Models/Outbound.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Models;
 
+use Log;
 use Illuminate\Database\Eloquent\Model;
 
 class Outbound extends Model
@@ -11,4 +12,55 @@ class Outbound extends Model
     {
         return $this->morphOne('Message', 'messageable');
     }
+
+    // Turn message to xml string so we can send them as response
+    public function xml()
+    { 
+        $xml = new SimpleXML('<xml/>');
+        array_map(function ($tag) use ($this) {
+            $xml->addChild(ucfirst($tag), $this->message()[$tag]);
+        }, ['toUserName', 'fromUserName', 'createTime', 'msgType']);
+
+        // add specific tags according to msg type.
+        // Note content fields are always capitalized
+        $type = $this->message()->msgType;
+        switch (true) {
+            case strstr($type, 'news'):
+                // most important message. we use them for arrival of new items
+                // and promotion event
+
+                break;
+            case strstr($type, 'text'):
+                $xml->addChild('Content', $this->content['Content']);
+                break;
+            case strstr($type, 'image'):
+                $xml->addChild('MediaId', $this->content['MediaId']);
+                break;
+            case strstr($type, 'voice'):
+                $xml->addChild('MediaId', $this->content['MediaId']);
+                break;
+            case strstr($type, 'video'):
+                Log::warning('Video outbound message is not implemented !');
+                break;
+            case strstr($type, 'music'):
+                Log::warning('Music outbound message is not implemented !');
+                break;
+            default:
+                Log::error('Not supported message type !');       
+        }
+
+        $dom = dom_import_simplexml($xml);
+        return $dom->ownerDocument->saveXML($dom->ownerDocument->documentElement);
+    }
+
+    public function getContentAttribute($value)
+    {
+        return json_decode($value);
+    }
+    
+    public function articles()
+    {
+        
+    }
+
 }
